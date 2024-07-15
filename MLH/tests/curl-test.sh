74536b5438e0b8d798cd0877426833549852c2e9
cuyakonwu <cuyakonwu@uchicago.edu>
2024-07-14 23:35:58 -0400

    Added correct files

---
+++ MLH/tests/curl-test.sh
@@ -0,0 +1,5 @@
+curl --request GET http://127.0.0.1:5000//api/timeline_post
+
+curl --request POST http://127.0.0.1:5000//api/timeline_post -d 'name=Conrad&email=cuyakonwu@mlh.io&content=Just Added Database to my portfolio site!'
+
+curl --request DELETE http://127.0.0.1:5000/api/timeline_post/1
