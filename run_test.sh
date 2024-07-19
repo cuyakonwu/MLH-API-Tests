4169ba71ed8388b41e34c627f61e0a34b3773959
cuyakonwu <cuyakonwu@uchicago.edu>
2024-07-19 16:41:26 -0400

    Update and rename curl-test.sh to run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+$PWD/python3-virtualenv/bin/python -m unittest discover -v tests/
+
 curl --request GET http://127.0.0.1:5000//api/timeline_post
 
 curl --request POST http://127.0.0.1:5000//api/timeline_post -d 'name=Conrad&email=cuyakonwu@mlh.io&content=Just Added Database to my portfolio site!'
